f542ea8d01dcdbc08496eba06fc725648451f041
domdneufeld <domdneufeld@gmail.com>
2018-05-15 15:20:04 -0600

    made menu and started happiness meter

--- george-bush-simulator - Dom/sketch.js
+++ george-bush-simulator - Dom/sketch.js
@@ -1,35 +1,58 @@
 let triviaQuestions = [
-  ["Rarely is the question asked, is our _____?", "Children Learning?", "Flag the coolest?", "Our country great?", "Neighbors sneaking?", 4, 2, 1, 3],
-  ["Our enemies are innovative and resourceful, and so are we. They never stop thinking about new ways to harm our country and our people, _____.", "And neither do we.", "But their leader isn’t nearly as handsome. ", "And frankly that’s just plain old unacceptable.", "And they must be stopped.", 4, 2, 3, 1],
+  ["Rarely is the question asked, is our _____?", "Children Learning?", "Flag the coolest?", "Our country great?", "Neighbors sneaking?", 1, 0, 0, 0],
+  ["Our enemies are innovative and resourceful, and so are we. They never stop thinking about new ways to harm our country and our people, _____.", "And frankly that’s just plain old unacceptable.", "But their leader isn’t nearly as handsome. ", "And neither do we.", "And they must be stopped.", 0, 0, 1, 0],
   [],
 ];
 
-let myTrivia;
+// Images
+let menuBackground;
+let angryBush; // radBush > gladBush > sadBush > angryBush
+let sadBush;
+let gladBush;
+let radBush;
 
-let state = 1;
-let gameState = 1;
+// Classes
+let myTrivia;
+let myMenu;
+let myTypingGame;
+
+let state = 0; //state 0 = menu, state 1 = trivia, state 2 = typing
 let triviaState = 1;
 //Variable for trivia game: 1 = top left 2 = top right, 3 = bottom left, 4 = bottom right;
 let buttonChoice;
 
+function preload() {
+  menuBackground = loadImage("images/startScreen.png");
+}
+
 function setup() {
   createCanvas(640, 640);
   myTrivia = new Trivia;
+  myTypingGame = new TypingGame;
+  myMenu = new Menu;
 }
 
 function draw() {
   background(200);
+  // Menu
+  if (state === 0) {
+    myMenu.displayBackground();
+    myMenu.displayStartButton();
+    myMenu.isMouseOverButton();
+  }
+  // Trivia
+  if (state === 1) {
+    myTrivia.isMouseOverButton();
+    if (triviaState === 1) {
+      myTrivia.displayButtons();
+      myTrivia.displayQuestion();
+    }
 
-  myTrivia.isMouseOverButton();
-  // Trivia game
-  if (triviaState === 1) {
-    myTrivia.displayButtons();
-    myTrivia.displayQuestion();
+    if (triviaState === 2) {
+      myTrivia.displayChoice();
+    }
   }
 
-  if (triviaState === 2) {
-    myTrivia.displayChoice();
-  }
 }
 
 class Trivia {
@@ -51,6 +74,8 @@ class Trivia {
     this.questiony = 360;
     // chooses the question
     this.triviaLevel = 0;
+    // Counts how many correct answers you got in a row
+    this.georgeBushHappiness = 0; //0 = angry, 1 = sad, 2 = glad, 3 = rad
   }
 
   displayButtons() {
@@ -183,27 +208,78 @@ class Trivia {
   displayChoice() {
     fill(0);
     textAlign(CENTER, CENTER);
-    text(triviaQuestions[this.triviaLevel][4 + this.buttonChoice], width / 2, height / 2);
+    text(this.georgeBushHappiness, width / 2, height / 2);
 
     // Next Question Button
     rectMode(CENTER);
-    fill(175, 0, 0);
+    fill(0, 0, 255);
     if (this.mouseOverNextQuestion) {
-      fill(255, 0, 0);
+      fill(0, 0, 200);
     }
     rect(320, 400, this.buttonWidth, this.buttonHeight);
-
     fill(0);
     text("Next Question", 320, 400);
+  }
+
+  changeHappiness(){
+    if (triviaQuestions[this.triviaLevel][4 + this.buttonChoice] === 1){
+      this.georgeBushHappiness += 1;
+    }
+    else if (triviaQuestions[this.triviaLevel][4 + this.buttonChoice] === 0 && this.georgeBushHappiness > 0){
+      this.georgeBushHappiness -= 1;
+    }
+  }
+}
+
+class TypingGame {
+
+}
+
+class Menu {
+  constructor() {
+    // Button variables
+    this.buttonWidth = 300;
+    this.buttonHeight = 150;
+    this.mouseOverButton = false;
+  }
+
+  displayBackground() {
+    image(menuBackground, 0, 0);
+  }
+
+  displayStartButton() {
+    fill(0, 0, 255);
+    if (this.mouseOverButton) {
+      fill(0, 0, 200);
+    }
+    stroke(4);
+    rectMode(CENTER);
+    rect(430, 200, this.buttonWidth, this.buttonHeight);
+
+    fill(0);
+    textSize(48);
+    textAlign(CENTER, CENTER);
+    text("Start Game", 430, 200);
+  }
+
+  isMouseOverButton() {
+    if (mouseX >= 430 - this.buttonWidth / 2 && mouseX <= 430 + this.buttonWidth / 2 &&
+      mouseY >= 200 - this.buttonHeight / 2 && mouseY <= 200 + this.buttonHeight / 2) {
+      this.mouseOverButton = true;
+    }
+    else {
+      this.mouseOverButton = false;
+    }
   }
 }
 
 function mousePressed() {
-  // Makes buttons clickable
-  if (state === 1 && gameState === 1 && triviaState === 1) {
+  // Makes buttons clickable in trivia game
+  if (state === 1 && triviaState === 1) {
     if (myTrivia.mouseOverButtonOne) {
       myTrivia.buttonChoice = 1;
       triviaState = 2;
+      myTrivia.changeHappiness();
     }
 
     if (myTrivia.mouseOverButtonTwo) {
@@ -221,10 +297,17 @@ function mousePressed() {
       triviaState = 2;
     }
   }
-  else if (state === 1 && gameState === 1 && triviaState === 2) {
+  else if (state === 1 && triviaState === 2) {
     if (myTrivia.mouseOverNextQuestion) {
       myTrivia.triviaLevel += 1;
       triviaState = 1;
+    }
+  }
+
+  // Makes button clickable on menu
+  if (state === 0) {
+    if (myMenu.mouseOverButton) {
+      state = 1;
     }
   }
 }
